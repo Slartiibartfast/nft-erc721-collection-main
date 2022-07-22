6de1a682996ae3a30a894b13de224d83860f99f5
Slartiibartfast <slartiibartfast13@gmail.com>
2022-07-22 01:39:19 -0400

    metadata.js

--- metadata.js
+++ metadata.js
@@ -1,10 +1,4 @@
-const fs = require('fs');
 
-const CID = `QmdMLQX78WbfHJtqUfGhsebXrTekaMZA5Bt63KTRbg29vC`;
-const TOKEN_COUNT = 200;
-fs.mkdirSync('metadata', { recursive: true });
-for (let i = 1; i <= TOKEN_COUNT; i++) {
-  const metadata = {
     const fs = require('fs');
 
 const CID = `QmdMLQX78WbfHJtqUfGhsebXrTekaMZA5Bt63KTRbg29vC`;
